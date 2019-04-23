3f3b406813806185e0a395fa7d637d87d2f0d252
Heather Hartley <hlhartley@gmail.com>
2019-04-22 22:14:41 -0600

    Adds buttons and description to projects

--- src/About.js
+++ src/About.js
@@ -5,10 +5,9 @@ class About extends Component {
 	render() {
 		return (
 			<div className="about-container">
-                <div className="about-title">About</div>
                 <div className="profile-container">
                     <img className="portrait-img" src={require("./assets/heatherhartley.jpeg")}/>
-                    <div className="about-text">I recently graduated from Turing School of Software and Design and am passionately pursuing a career in software engineering. I have been acknowledged for creating visually-appealing as well as interactive web-based apps. Some of my featured projects and designs can be found below and on GitHub.</div>
+                    <div className="about-text">I recently graduated from Turing School of Software and Design and am passionately pursuing a career in software engineering. I have been acknowledged for creating interactive as well as visually-appealing web-based apps. Some of my featured projects and designs can be found below and on GitHub.</div>
                 </div>
             </div>
 		);
--- src/Projects.js
+++ src/Projects.js
@@ -6,10 +6,55 @@ class Projects extends Component {
 		return (
             <div>
                 <div className="projects-container">
-                {/* <div className="projects-title">Projects</div> */}
+                    <div className="project">
                         <img className="project-img" src={require("./assets/terminal-commander.png")}/>
-                    <img className="project-img" src={require("./assets/seasonal-shades.png")}/>
-                    <img className="project-img" src={require("./assets/listy.png")}/>
+                        <div className="project-text-right">A web-based React game that allows users to enhance muscle memory by repetitively typing actual terminal commands into the input box.
+                            <ul>
+                                <li>React</li>
+                                <li>SCSS</li>
+                                <li>Jest + Enzyme</li>
+                            </ul>
+                            <div className="buttons-container">
+                                <button>GitHub</button>
+                                <button>App Link</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="project">
+                        <div className="project-text-left">A web-based React website designed to give makeup lovers color ideas who want to find the perfect shade of lipstick, blush, eyeshadow, or nailpolish.
+                            <ul>
+                                <li>React</li>
+                                <li>Redux</li>
+                                <li>React Router</li>
+                                <li>JavaScript</li>
+                                <li>SCSS</li>
+                                <li>Jest + Enzyme</li>
+                            </ul>
+                            <div className="buttons-container">
+                                <button>GitHub</button>
+                                <button>App Link</button>
+                            </div>
+                        </div>
+                        <img className="project-img" src={require("./assets/seasonal-shades.png")}/>
+                    </div>
+                    <div className="project">
+                        <img className="project-img" src={require("./assets/listy.png")}/>
+                        <div className="project-text-right">An app similar to Google Keep created with both front-end and back-end frameworks which allows users to save, edit, and delete notes.
+                            <ul>
+                                <li>React</li>
+                                <li>Redux</li>
+                                <li>React Router</li>
+                                <li>JavaScript</li>
+                                <li>SCSS</li>
+                                <li>Express</li>
+                                <li>Jest + Supertest</li>
+                            </ul>
+                            <div className="buttons-container">
+                                <button>GitHub</button>
+                                <button>App Link</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
 		);
