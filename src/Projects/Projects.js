103f3ead4447cc480752710fae071a940bb040cd
Heather Hartley <hlhartley@gmail.com>
2019-05-05 22:50:08 -0600

    Adds more links to deployed apps on AWS and adds Wheel of Fortune to projects

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -108,10 +108,11 @@ class Projects extends Component {
                                 <li>Makeup API</li>
                                 <li>SCSS</li>
                                 <li>Jest + Enzyme</li>
+                                <li>Amazon Web Services - S3</li>
                             </ul>
                             <div className="buttons-container">
                                 <a href="https://github.com/hlhartley/seasonal-shades" target="_blank" className="button-link">GitHub</a>
-                                <div className="disabled-button">App</div>
+                                <a href="http://seasonal-shades.s3-website-us-west-2.amazonaws.com/eyeshadow" target="_blank" className="button-link">App</a>
                             </div>
                         </div>
                         <a href="https://github.com/hlhartley/seasonal-shades" target="_blank"><img className="project-img" src={require("../assets/seasonal-shades.png")}/></a>
@@ -125,14 +126,15 @@ class Projects extends Component {
                             <li>Starwars API</li>
                             <li>SCSS</li>
                             <li>Jest + Enzyme</li>
+                            <li>Amazon Web Services - S3</li>
                         </ul>
                         <div className="buttons-container">
                             <a href="https://github.com/hlhartley/swapi-box" target="_blank" className="button-link">GitHub</a>
+                            <a href="http://swapi-box.s3-website-us-west-2.amazonaws.com/" target="_blank" className="button-link">App</a>
                             <div className="disabled-button">App</div>
                         </div>
                     </div>
                 </div>
-                
                 <div className="project">
                     <div className="project-text-left">You can easily see all of the ice cream flavors available throughout Denver and Aurora as well as explore and filter for new and exciting flavors.
                         <ul>
@@ -141,18 +143,35 @@ class Projects extends Component {
                             <li>Fetch API</li>
                             <li>SCSS</li>
                             <li>Jest + Enzyme</li>
+                            <li>Amazon Web Services - S3</li>
                         </ul>
                         <div className="buttons-container">
                             <a href="https://github.com/hlhartley/icecream_whateverly" target="_blank" className="button-link">GitHub</a>
+                            <a href="http://icecream-whateverly.s3-website-us-west-2.amazonaws.com/" target="_blank" className="button-link">App</a>
                             <div className="disabled-button">App</div>
                         </div>
                     </div>
                     <a href="https://github.com/hlhartley/icecream_whateverly" target="_blank"><img className="project-img" src={require("../assets/iscream.png")}/></a>
                 </div>
                 <div className="project">
+                    <a href="https://github.com/hlhartley/wheeloffortune" target="_blank"><img className="project-img" src={require("../assets/wheeloffortuness.png")}/></a>
+                        <div className="project-text-right">$pin 2 !win is a take on the classic game show Wheel of Fortune. Players can test their luck by guessing phrases and winning a variety of monetary prizes through four rounds of fun. This web app was built using Object Oriented Principles in Javascript.
+                            <ul>
+                                <li>JavaScript + OOP</li>
+                                <li>CSS</li>
+                                <li>Mocha + Chai</li>
+                                <li>Amazon Web Services - S3</li>
+                            </ul>
+                            <div className="buttons-container">
+                                <a href="https://github.com/hlhartley/wheeloffortune" target="_blank" className="button-link">GitHub</a>
+                                <a href="http://spin-2-win.s3-website-us-west-2.amazonaws.com/" target="_blank" className="button-link">App</a>
+                            </div>
+                        </div>
+                    </div>
+                <div className="project">
                     <a href="https://github.com/hlhartley/PicYourLandmark" target="_blank"><img className="project-phone-img" src={require("../assets/picyourlandmarklogin.png")}/></a>
                     <a href="https://github.com/hlhartley/PicYourLandmark" target="_blank"><img className="project-phone-img" src={require("../assets/picyourlandmark.png")}/></a>
-                    <div className="project-phone-text-right">Based on the famous game 'Pokemon Go' - except you can 'collect' famous landmarks! When you visit a landmark, you can take and store your picture in the app. You gain points/gems based on how many landmarks you have 'collected'.
+                    <div className="project-phone-text-right">Based on the famous game 'Pokemon Go' - except you can 'collect' famous landmarks! When you visit a landmark, you can take and store your picture in the app. You gain points/gems based on how many landmarks you have 'collected'. Download our app from Google Play store on your Android mobile device today!
                         <ul>
                             <li>React Native</li>
                             <li>SCSS</li>
@@ -161,10 +180,11 @@ class Projects extends Component {
                             <li>Rest_framework</li>
                             <li>PostgreSQL</li>
                             <li>Jest + Enzyme</li>
+                            <li>Google Play store</li>
                         </ul>
                         <div className="buttons-container">
                             <a href="https://github.com/hlhartley/PicYourLandmark" target="_blank" className="button-link">GitHub</a>
-                            <div className="disabled-button">App</div>
+                            <a href="https://play.google.com/store/apps/details?id=com.heatherandmatt.picyourlandmark" target="_blank" className="button-link">App</a>
                         </div>
                     </div>
                 </div>
